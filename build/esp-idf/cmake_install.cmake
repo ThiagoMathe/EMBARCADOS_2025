f3dc1c74926db375d7973f5ad85fae6e7dd886fc
Jeremias Marques <jeremias.samuel10@gmail.com>
2025-03-05 19:39:05 -0300

    Decimo Commit

--- build/esp-idf/cmake_install.cmake
+++ build/esp-idf/cmake_install.cmake
@@ -567,3 +567,8 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("C:/Users/jerem/EMBARCADOS_2025/build/esp-idf/main/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/jerem/EMBARCADOS_2025/build/esp-idf/s12/cmake_install.cmake")
+endif()
+
